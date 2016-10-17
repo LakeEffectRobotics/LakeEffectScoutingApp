e3c27869488382cd2c6b374d0bbd440e2dba4502
ramachandra <ajay@ajayinkingston.com>
2016-10-17 08:36:13 -0400

    made dialog box actually work, I think

--- app/src/main/java/ca/lakeeffect/scoutingapp/EndgamePage.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/EndgamePage.java
@@ -1,5 +1,6 @@
 package ca.lakeeffect.scoutingapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -7,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.Toast;
 
 public class EndgamePage extends Fragment implements View.OnClickListener{
 
@@ -29,11 +30,37 @@ public class EndgamePage extends Fragment implements View.OnClickListener{
 
     public void onClick(View v){
         if(v==submit){
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("Are you sure you want to submit?")
-                    .setTitle("Confirm Submission");
-            AlertDialog dialog = builder.create();
-//            https://developer.android.com/guide/topics/ui/dialogs.html
+            new AlertDialog.Builder(getActivity())
+                .setTitle("Submiting")
+                .setMessage("Are you sure you would like to submit?")
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(),
+                                "You hit no", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setPositiveButton(android.R.string.yes,  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();Toast.makeText(getActivity(),
+                                "Your Message", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .create();
         }
     }
 }
+
+
