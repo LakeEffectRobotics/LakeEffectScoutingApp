2ae562b2f5597283ad5e9f0f0e9bd5b7bfa23d29
Aree Vanier <gregory@ajayinkingston.com>
2017-01-16 18:53:19 -0500

    Fixed higher counter

--- app/src/main/java/ca/lakeeffect/scoutingapp/HigherCounter.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/HigherCounter.java
@@ -6,12 +6,22 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class HigherCounter extends Counter{
+import java.util.ArrayList;
+import java.util.List;
 
+public class HigherCounter extends LinearLayout implements View.OnClickListener{
 
+    TextView counterText;
 
+    int count;
+
+    List<Long> times = new ArrayList<>();
+
+    Button plusOneButton;
+    Button minusOneButton;
     Button plus5Button;
     Button minus5Button;
 
@@ -22,7 +32,11 @@ public class HigherCounter extends Counter{
         inflater.inflate(R.layout.higher_counter, this);
 
         plus5Button = (Button) findViewById(R.id.plus5Button);
-        plus5Button.setOnClickListener(this);
+        plus5Button.setOnClickListener(this);        
+        plusOneButton = (Button) findViewById(R.id.plusOneButton);
+        plusOneButton.setOnClickListener(this);
+        minusOneButton = (Button) findViewById(R.id.minusOneButton);
+        minusOneButton.setOnClickListener(this);
         minus5Button = (Button) findViewById(R.id.minus5Button);
         minus5Button.setOnClickListener(this);
 
@@ -37,6 +51,7 @@ public class HigherCounter extends Counter{
         else if(view == plus5Button) count +=5;
         if(view == minusOneButton) count --;
         else if(view == minus5Button) count -=5;
+        if(count < 0) count = 0;
         counterText.setText(count+"");
     }
 
