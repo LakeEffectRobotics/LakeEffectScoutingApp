d245a9ee4ba3f17ef834ea2fef9ebef7ee1f0a99
ramachandra <ajay@ajayinkingston.com>
2017-10-10 19:57:50 -0400

    fixed error

--- app/src/main/java/ca/lakeeffect/scoutingapp/ConnectionThread.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/ConnectionThread.java
@@ -92,7 +92,7 @@ public class ConnectionThread implements Runnable {
 
     public void sendLabels(){
         try {
-            this.out.write((mainActivity.robotNum + ":" + mainActivity.getData()[1]).getBytes(Charset.forName("UTF-8")));
+            this.out.write((mainActivity.robotNum + ":" + mainActivity.getData(true)[1]).getBytes(Charset.forName("UTF-8")));
         } catch (IOException e) {
             e.printStackTrace();
         }
