968b1101a8b6fff1190a4c89d1d0c8c2cf5f173f
= <ajay@ajayinkingston.com>
2018-03-24 11:13:26 -0400

    Changed build number and switched Vault Hit back to Vault Cubes

--- app/src/main/java/ca/lakeeffect/scoutingapp/TeleopPage.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/TeleopPage.java
@@ -240,7 +240,7 @@ public class TeleopPage extends Fragment implements View.OnClickListener {
         data.append(otherSwitchHit+",");
         labels.append("Other Switch Miss,");
         data.append(otherSwitchMiss+",");
-        labels.append("Vault Hit,");
+        labels.append("Vault Cubes,");
         data.append(vaultHit+",");
         labels.append("Vault Miss,");
         data.append(vaultMiss+",");
