c014ae61b0dcdb61ad196440fb6f30b81428fc07
Ajay Ramachandran <ajay@ajay.app>
2019-02-04 12:17:06 -0500

    Put back Xan's Pre-Game layout

--- app/src/main/java/ca/lakeeffect/scoutingapp/PregamePage.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/PregamePage.java
@@ -6,7 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by Ajay on 9/25/2016.
@@ -23,12 +27,55 @@ public class PregamePage extends Fragment {
 
         View view = inflator.inflate(R.layout.pregamepage, container, false);
 
-        Spinner startSpinner = view.findViewById(R.id.startPositionSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.startPosition, R.layout.spinner);
-        startSpinner.setAdapter(adapter);
-
         view.setTag("page1");
 
+        Spinner spinner = view.findViewById(R.id.autoStartLocation);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.startPosition, R.layout.spinner);
+        spinner.setAdapter(adapter);
+
+        final RadioButton success = (RadioButton) view.findViewById(R.id.leftHabSuccess);
+
+        final RadioButton fail = (RadioButton) view.findViewById(R.id.leftHabFail);
+
+        success.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if(checked){
+                    fail.setChecked(false);
+                }
+            }
+        });
+
+        fail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if(checked){
+                    success.setChecked(false);
+                }
+            }
+        });
+
+        final CheckBox cargo = (CheckBox) view.findViewById(R.id.startingObjectsCargo);
+
+        final CheckBox hatch = (CheckBox) view.findViewById(R.id.startingObjectsHatch);
+
+        cargo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if(checked){
+                    hatch.setChecked(false);
+                }
+            }
+        });
+
+        hatch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if(checked){
+                    cargo.setChecked(false);
+                }
+            }
+        });
 
         return view;
 
