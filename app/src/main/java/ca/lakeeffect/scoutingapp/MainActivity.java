311e201bca1f3a21e437a72e356bb7c02cfab116
Ajay Ramachandran <dev@ajay.app>
2019-02-24 11:48:30 -0500

    Fixed auto data not being sent and encoded nodata properly.
    Resolves https://github.com/LakeEffectRobotics/LakeEffectScoutingApp/issues/228

--- app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
+++ app/src/main/java/ca/lakeeffect/scoutingapp/MainActivity.java
@@ -263,49 +263,13 @@ public class MainActivity extends AppCompatActivity {
             allEvents = pagerAdapter.teleopPage.events;
         }
 
-        for(Event event : pagerAdapter.teleopPage.events){
+        for(Event event : allEvents){
             int location = event.location;
 
             events.append(matchNumber + "," + event.eventType + "," + location + "," + event.timestamp + "," + event.metadata + "\n");
         }
 
         return Base64.encodeToString(events.toString().getBytes(Charset.forName("UTF-8")), Base64.DEFAULT);
-    }
-
-    //will return the same location but on the other side of the field
-    public static int flipLocation(int location){
-        switch (location){
-            case 0:
-                return 11;
-            case 1:
-                return 12;
-            case 2:
-                return 13;
-            case 3:
-                return 10;
-            case 4:
-                return 8;
-            case 5:
-                return 9;
-            case 6:
-                return 6;
-            case 7:
-                return 7;
-            case 8:
-                return 4;
-            case 9:
-                return 5;
-            case 10:
-                return 3;
-            case 11:
-                return 0;
-            case 12:
-                return 1;
-            case 13:
-                return 2;
-        }
-
-        return 0;
     }
 
     //updates the view showing the matches left until this scout is off
@@ -649,7 +613,7 @@ public class MainActivity extends AppCompatActivity {
             String fulldata = "";
             if (!teleOpEvents.equals("")) {
                 if (autoEvents.equals("")) {
-                    autoEvents = "nodata";
+                    autoEvents = Base64.encodeToString("nodata".getBytes(Charset.forName("UTF-8")), Base64.DEFAULT);
                 }
                 fulldata = robotNum + ":" + data[0] + ":" + autoEvents + ":" + teleOpEvents;
             } else if(!autoEvents.equals("")) {
